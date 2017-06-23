30689327618a39c6f883e56987475ab6be0e3a6e
t.reinartz <t.reinartz@maxxton.com>
2017-06-23 15:27:40 +0200

    improve auth service.

--- jsonapi/core/core/AuthService.php
+++ jsonapi/core/core/AuthService.php
@@ -33,40 +33,40 @@ use \Firebase\JWT\JWT;
  */
 class AuthService extends \JsonApi\Core
 {
-  public $slim;
-  private $config;
+    public $slim;
+    private $config;
 
-  protected $routes = [];
+    protected $routes = [];
 
-  private $JWT_KEY = "MosXCKe:T_r6tu-jkZyX0,swz)[AgQJi0TpD-|n`NE]Zb]XgvAnL?b+0uC)~[U";
-  /**
-   * app constructor.
-   */
-  public function __construct($config)
-  {
-    $this->config = $config;
-    parent::__construct($config);
+    private $JWT_KEY = "MosXCKe:T_r6tu-jkZyX0,swz)[AgQJi0TpD-|n`NE]Zb]XgvAnL?b+0uC)~[U";
+    /**
+     * app constructor.
+     */
+    public function __construct($config)
+    {
+        $this->config = $config;
+        parent::__construct($config);
 
-    if(isset($config['JWT_KEY'])){
-      $this->JWT_KEY = $config['JWT_KEY'];
+        if(isset($config['JWT_KEY'])){
+            $this->JWT_KEY = $config['JWT_KEY'];
+        }
+
+        $this->setRoutes();
     }
 
-    $this->setRoutes();
-  }
+    private function setRoutes(){
 
-  private function setRoutes(){
+        $authService = $this;
 
-    $authService = $this;
+        //handle request to check token
+        $this->slim->get('/is-authorized/{token}', function ($request, $response, $args) use ($authService) {
+            return $authService->handleIsAuthRequest($request, $response, $args);
+        });
 
-    //handle request to check token
-    $this->slim->get('/is-authorized/{token}', function ($request, $response, $args) use ($authService) {
-      return $authService->handleIsAuthRequest($request, $response, $args);
-    });
-
-    //handle request to create token
-    $this->slim->get('/authorize', function ($request, $response, $args) use ($authService) {
-      return $authService->handleLoginRequest($request, $response, $args);
-    });
+        //handle request to create token
+        $this->slim->post('/token', function ($request, $response, $args) use ($authService) {
+            return $authService->handleLoginRequest($request, $response, $args);
+        });
 
 
 
@@ -101,130 +101,163 @@ class AuthService extends \JsonApi\Core
 //        'requestHandler' => function($config, $request, $response, $args){return $this->handleUsersRequest($config, $request, $response, $args);}
 //      ],
 //    ];
-  }
-
-  public function run(){
-    $this->slim->run();
-  }
-
-  protected function setupFromConfig()
-  {
-    $this->setupSlimRoutes($this->routes);
-  }
-
-  protected function getExpireDate(){
-    return date("Y-m-d H:i:s", strtotime('+1 day'));
-  }
-
-
-  protected function handleLoginRequest($request, $response, $args){
-
-    $parsedBody = $request->getParsedBody();
-    if( isset($parsedBody['username']) && isset($parsedBody['password']) ){
-      $database = new \JsonApi\DatabaseHelpers($this->config['database_config']);
-      //get user from db.
-      $database->getRecordInTable('users', []);
-      //check password with salt.
-
-      //return token or unauthorized
-    } else {
-      return $response->withStatus(400);
     }
 
-    $user = $database->getRecordInTable('users', ['userId', 'username', 'lastlogin'], 'userId', $session['userId']);
-
-
-    //return token.
-    $response->write(json_encode($this->createToken([], 1)));
-
-    return $response;
-
-  }
-
-  protected function handleIsAuthRequest($request, $response, $args){
-    $user = null;
-    if(isset($args['token'])){
-      $database = new \JsonApi\DatabaseHelpers($this->config['database_config']);
-      $session = $database->getRecordInTable('sessions', ['userId'], 'token', $args['token']);
-      if( !is_null($session ) ){
-        $user = $database->getRecordInTable('users', ['userId', 'username', 'lastlogin'], 'userId', $session['userId']);
-      }
+    public function run(){
+        $this->slim->run();
     }
 
-    if(!is_null($user)){
-      $response->withStatus(200)->write(json_encode($user));
-    } else {
-      $response->withStatus(500);
+    protected function setupFromConfig()
+    {
+        $this->setupSlimRoutes($this->routes);
     }
 
-    return $response;
-  }
+    protected function getExpireDate(){
+        return date("Y-m-d H:i:s", strtotime('+1 day'));
+    }
 
-  protected function handleUsersRequest($config, $request, $response, $next){
-    if($request->isPost()){
-      $user = $request->getParsedBody();
-      var_dump($user);
-      //creation of users is only allowed with a password object.
-      if(!isset($user['password'])){
+
+    protected function handleLoginRequest($request, $response, $args){
+
+//    $parsedBody = $request->getParsedBody();
+//      https://oauth.example.com/token?grant_type=client_credentials&client_id=CLIENT_ID&client_secret=CLIENT_SECRET
+
+        $queryParams = $request->getQueryParams();
+        if(!isset($queryParams['grant_type'])){
+            return $response->withStatus(400);
+        }
+
+
+        if( $queryParams['grant_type'] == 'client_credentials' && isset($queryParams['client_id'])&& isset($queryParams['client_secret']) ){
+            $database = new \JsonApi\DatabaseHelpers($this->config['database_config']);
+            //get user from db.
+            $userSession = $database->getRecordInTable('users', '*', 'clientId', $queryParams['client_id']);
+            //check secret with salt.
+            if($userSession != null){
+
+                $hashedSecretParam = $this->generateHash($queryParams['client_secret'], $userSession['salt']);
+                $clientSecret = $userSession['clientSecret'];
+                //check if user password is correct.
+                if($hashedSecretParam == $clientSecret){
+                    $user = $userSession;
+                    unset($user['salt']);
+                    unset($user['clientSecret']);
+                    return $response->write(json_encode($this->createToken($userSession,1 )));
+                } else {
+                    return $response->withStatus(401)->write(json_encode($hashedSecretParam));
+                }
+
+            }
+            //return token or unauthorized
+        }
+        return $response->withStatus(400);
+    }
+
+    protected function handleIsAuthRequest($request, $response, $args){
+        $user = null;
+        if(isset($args['token'])){
+            $database = new \JsonApi\DatabaseHelpers($this->config['database_config']);
+            $session = $database->getRecordInTable('sessions', ['userId'], 'token', $args['token']);
+            if( !is_null($session ) ){
+                $user = $database->getRecordInTable('users', ['userId', 'username', 'lastlogin'], 'userId', $session['userId']);
+            }
+        }
+
+        if(!is_null($user)){
+            $response->withStatus(200)->write(json_encode($user));
+        } else {
+            $response->withStatus(500);
+        }
+
+        return $response;
+    }
+
+    protected function handleUsersRequest($config, $request, $response, $next){
+        if($request->isPost()){
+            $user = $request->getParsedBody();
+            var_dump($user);
+            //creation of users is only allowed with a password object.
+            if(!isset($user['password'])){
 //        return $response->withStatus(400);
-      }
+            }
+        }
+
+        /**
+         * parse password to an encrypted variable
+         */
+        if( $request->isPost() OR ( $request->isPut() AND isset($user['password']) ) ){
+            $user = $request->getParsedBody();
+            array_push($config['columns'], 'password');
+            array_push($config['columns'], 'secret');
+
+            $secret = $this->generateHash();
+            $password = hash_hmac('ripemd160', $user['password'], $secret);
+
+            $user['secret'] = $secret;
+            $user['password'] = $password;
+            var_dump($user);
+
+            $request->getBody()->write(json_encode($user));
+        }
+
+        $response = $next($request, $response);
+        return $response;
+//    return $this->dataBaseMiddleWare($config, $request, $response, $next);
     }
 
     /**
-     * parse password to an encrypted variable
+     * simple function to generate a hash.
      */
-    if( $request->isPost() OR ( $request->isPut() AND isset($user['password']) ) ){
-      $user = $request->getParsedBody();
-      array_push($config['columns'], 'password');
-      array_push($config['columns'], 'secret');
+    private function generateHash($hashString, $salt) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $hashData = '';
 
-      $secret = $this->generateHash();
-      $password = hash_hmac('ripemd160', $user['password'], $secret);
 
-      $user['secret'] = $secret;
-      $user['password'] = $password;
-      var_dump($user);
+        if(!isset($hashString) and !is_null($hashData)){
+            $randomString = '';
+            for ($i = 0; $i < 50; $i++) {
+                $hashData .= $characters[rand(0, $charactersLength - 1)];
+            }
+            $hashData = date('Y-m-d H:i:s') +  $randomString +  microtime(true);
+        } else {
+            $hashData = $hashString;
+        }
 
-      $request->getBody()->write(json_encode($user));
+        $hash = hash('ripemd160', $hashData);
+        if(isset($salt)){
+            $hash = hash('ripemd160', $hash + $salt);
+            $hash = hash('ripemd160', $hash + $salt);
+        }
+
+        return $hash;
     }
 
-    $response = $next($request, $response);
-    return $response;
-//    return $this->dataBaseMiddleWare($config, $request, $response, $next);
-  }
 
-  /**
-   * simple function to generate a hash.
-   */
-  private function generateHash() {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
 
-    for ($i = 0; $i < 50; $i++) {
-      $randomString .= $characters[rand(0, $charactersLength - 1)];
+    private function createToken($user){
+        $expireDate = $this->getExpireDate();
+        $token = [
+            "expiresOn" => $expireDate,
+            "user" => $user,
+        ];
+        $jwt = JWT::encode($token, $this->JWT_KEY);
+        return $jwt;
     }
 
-    $hash = hash('ripemd160', date('Y-m-d H:i:s') +  $randomString +  microtime(true));
+    private function getTokenObject($user){
+        $data = [];
+        $data['access_token'] = $this->createToken($user);
+        $data['token_type'] = "bearer";
+        $data['expires_in'] = 0;
+        $data['scope'] = "all";
+        $data['uid'] = $user['clientId'];
+        $data['info'] = $user;
 
-    return $hash;
-  }
+}
 
-
-
-  private function createToken($user, $tokenId){
-    $expireDate = $this->getExpireDate();
-    $token = [
-      "expiresOn" => $expireDate,
-      "user" => $user,
-      "tokenId" => $tokenId,
-    ];
-    $jwt = JWT::encode($token, $this->JWT_KEY);
-    return $jwt;
-  }
-
-  private function decodeToken($jwt){
-    return (array) JWT::decode($jwt, $this->JWT_KEY, array('HS256'));
-  }
+    private function decodeToken($jwt){
+        return (array) JWT::decode($jwt, $this->JWT_KEY, array('HS256'));
+    }
 
 }
